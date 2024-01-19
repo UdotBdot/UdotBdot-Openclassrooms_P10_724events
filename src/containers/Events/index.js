e391018bc7ee30fba9dc39255a657889759f4dc7
UdotBdot <Frederic.sin@proton.me>
2024-01-19 16:49:25 +0100

    FIX: Filtre

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -7,34 +7,34 @@ import ModalEvent from "../ModalEvent";
 
 import "./style.css";
 
-const PER_PAGE = 9;
+const perPage = 9;
 
 const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = (
-    (!type
-      ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
-    if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
-    ) {
-      return true;
-    }
-    return false;
-  });
+
+  const startIdx = (currentPage - 1) * perPage;
+  const endIdx = currentPage * perPage;
+
+  const filteredEvents =
+    (type
+      ? data?.events.filter((event) => event.type === type)
+      : data?.events) || [];
+
+  const paginatedEvents = filteredEvents.slice(startIdx, endIdx);
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
-  };
-  const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
+  };  
+
+  const pageNumber = Math.ceil(filteredEvents.length / perPage);
   const typeList = new Set(data?.events.map((event) => event.type));
+
   return (
     <>
-      {error && <div>An error occured</div>}
+      {error && <div>An error occurred</div>}
       {data === null ? (
         "loading"
       ) : (
@@ -45,7 +45,7 @@ const EventList = () => {
             onChange={(value) => (value ? changeType(value) : changeType(null))}
           />
           <div id="events" className="ListContainer">
-            {filteredEvents.map((event) => (
+            {paginatedEvents.map((event) => (
               <Modal key={event.id} Content={<ModalEvent event={event} />}>
                 {({ setIsOpened }) => (
                   <EventCard
@@ -60,9 +60,9 @@ const EventList = () => {
             ))}
           </div>
           <div className="Pagination">
-            {[...Array(pageNumber || 0)].map((_, n) => (
+            {[...Array(pageNumber)].map((_, n) => (
               // eslint-disable-next-line react/no-array-index-key
-              <a key={n} href="#events" onClick={() => setCurrentPage(n + 1)}>
+              <a key={n + 1} href="#events" onClick={() => setCurrentPage(n + 1)}>
                 {n + 1}
               </a>
             ))}
